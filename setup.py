423c951899033e33ce3ffd7a8a88880df703b378
mwendwac <mwendwa@mesozi.com>
2016-11-10 10:18:09 +0300

    Version 1.0:Version with all functionalities working

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='todopackage',
-    version='0.1',
+    name='todo',
+    version='1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -10,19 +10,20 @@ setup(
         'requests',
         'python-firebase',
         'tabulate',
-        'colorama'
+        'colorama',
+        'pyfiglet'
     ],
     entry_points='''
         [console_scripts]
-        todopackage=todopackage.scripts.script:todo
-        todo=todopackage.scripts.script:todo
-        open=todopackage.scripts.script:open
-        create=todopackage.scripts.script:create
-        list=todopackage.scripts.script:mylist
-        items=todopackage.scripts.script:items
-        item=todopackage.scripts.script:item
-        add=todopackage.scripts.script:add
-        cli=todopackage.scripts.script:cli
+        todo=todo.scripts.main:Todo.todo
+        open=todo.scripts.main:Todo.open
+        create=todo.scripts.main:Todo.create
+        list=todo.scripts.main:Todo.mylist
+        items=todo.scripts.main:Todo.items
+        item=todo.scripts.main:Todo.item
+        add=todo.scripts.main:Todo.add
+        cli=todo.scripts.main:Todo.cli
+        start_todo = todo.scripts.main:Todo.run_app
 
       ''',
 )
--- todo/scripts/access_var.py
+++ todo/scripts/access_var.py
@@ -1,16 +1,16 @@
 def readStatus():
-	with open('vars.txt') as file:
+	with open('temp/vars.txt') as file:
 		status = file.readlines()[-1]
 		return status
 
 
 def writeStatus(stat):
-	my_file = open("vars.txt", "w")
+	my_file = open('temp/vars.txt', "w")
 	my_file.write(stat) 
 	my_file.close()
 
 def readLastOpen():
-	with open('read.txt') as file:
+	with open('temp/read.txt') as file:
 		status = file.readlines()[-1]
 		try:
 			return int(status)
@@ -19,6 +19,6 @@ def readLastOpen():
 
 def writeLastOpen(last):
 	last =str(last)
-	my_file = open("read.txt", "w")
+	my_file = open('temp/read.txt', "w")
 	my_file.write(last) 
 	my_file.close()
