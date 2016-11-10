6e044247589727eeb1cfc245b3f0e29880342a32
mwendwac <mwendwa@mesozi.com>
2016-11-10 21:22:40 +0300

    fire base

--- todo/fire_base.py
+++ todo/fire_base.py
@@ -1,9 +1,13 @@
 from firebase import firebase
 
 def synchronize(ffb_data):
+	"""
+	Synchronizes database to remote firebase storage
+	"""
 	try:
 		fbase = firebase.FirebaseApplication('https://todo-823db.firebaseio.com/', None)
 		mydata = ffb_data
-		result = fbase.post('/', mydata,connection=None,params = {'print': 'pretty'},headers= {'X_FANCY_HEADER': 'VERY FANCY'})
+		fbase.post('/',mydata,connection=None,params ={'print': 'pretty'},
+			headers={'X_FANCY_HEADER': 'VERY FANCY'})
 	except:
 		return "connection error"
